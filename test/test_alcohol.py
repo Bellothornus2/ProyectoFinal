47912186fd926692a5288a48d4a39f063bcd7e5b
bellothornus2 <divanov@cifpfbmoll.eu>
2022-05-30 22:34:48 +0200

    feat: :zap: we shortened th work required to do the tests

--- test/test_alcohol.py
+++ test/test_alcohol.py
@@ -6,19 +6,13 @@ from services.alcohol import get_alcohol_last_id
 @pytest.mark.create_alcohol ##invocar con pytest - create_alcohol
 def test_add_alcohol():
 	client_api = app.test_client()
-	result = client_api.post("/alcohol",json={
-    "name":"Cerveza Generica 2",
-    "alcohol_grade": 2.5,
-    "price": 1.80,
-    "brand": "Cosas",
-    "type_alcohol_id":1,
-    "subtype_alcohol_id":1,
-    "description":"Prueba de Creacio de Cerveza Pale Ale"
-	})
+	alcohol_file = open("./utilities/alcohol.json")
+	alcohol_data = json.load(alcohol_file)
+	result = client_api.post("/alcohol",json=alcohol_data["create"])
 	last_id = get_alcohol_last_id()
 	last_alcohol = client_api.get("alcohol/%s" % (last_id))
 	assert result.data == last_alcohol.data
-	client_api.delete("/alcohol/%s" % (last_id))
+	#client_api.delete("/alcohol/%s" % (last_id))
 
 @pytest.mark.alcohol_last_id ##invocar con pytest - alcohol_last_id
 def test_alcohol_last_id():
